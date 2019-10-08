f10dc65f3d61a32a8206631891fa824618fa3350
aaronmartinez <aaroncmartinez@ldschurch.org>
2019-10-07 18:57:45 -0600

    Add an observer to fire when the secondary view closes. Bump pod version

--- PaneViewController/PaneViewController.swift
+++ PaneViewController/PaneViewController.swift
@@ -54,6 +54,7 @@ open class PaneViewController: UIViewController {
     public let secondaryViewController: UIViewController
     public let primaryViewWillChangeWidthObservers = Signals.Signal<UIView>()
     public let primaryViewDidChangeWidthObservers = Signals.Signal<UIView>()
+    public let secondaryViewDidCloseObservers = Signals.Signal<Void>()
     public weak var delegate: PaneViewControllerDelegate?
     
     public private(set) var presentationMode = PresentationMode.modal
@@ -532,6 +533,8 @@ open class PaneViewController: UIViewController {
             case .modal:
                 break
             }
+
+            self.secondaryViewDidCloseObservers.fire()
         })
     }
     
